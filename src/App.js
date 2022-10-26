ffdce7cbaa860d3241c0e0fde34fb55124e23bca
glody007 <glodymbutwile@gmail.com>
2022-10-26 14:57:28 +0200

    feat: Hero section.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import './App.scss';
+import logo from './assets/logo.svg'
 import { Header } from './component';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='App-section Hero'>
+        <div className='Hero-main'>
+          <h1>Understand how <span>Blockchain</span> works on just one page.</h1>
+          <h4>Learn how Bitcoin works by understanding the blockchain, which is the technology behind it.</h4>
+          <button className='button button--cta'>
+            Learn
+          </button>
+        </div>
+        <div className='Hero-image'>
+          <img src={logo} alt="Blockchain illustration" />
+        </div>
+      </div>
     </div>
   );
 }
