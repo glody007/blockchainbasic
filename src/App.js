1776a5b268fa3a7560d33f46bae296e59d23e0e1
glody007 <glodymbutwile@gmail.com>
2022-10-26 11:45:33 +0200

    refactor: Create Header component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import './header.scss'
+import { Header } from './component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <nav className="Header-nav">
-          <img src={logo} className="Header-logo" alt="logo" />
-          <ul className="Header-links">
-            <li className="Header-link">Hash</li>
-            <li className="Header-link">Block</li>
-            <li className="Header-link">Blockchain</li>
-            <li className="Header-link">Distributed</li>
-            <li className="Header-link">Tokens</li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/component/header.js
@@ -0,0 +1,21 @@
+import logo from '../assets/logo.svg';
+import './header.scss'
+
+function Header() {
+  return (
+    <header className="App-header">
+        <nav className="Header-nav">
+            <img src={logo} className="Header-logo" alt="logo" />
+            <ul className="Header-links">
+            <li className="Header-link">Hash</li>
+            <li className="Header-link">Block</li>
+            <li className="Header-link">Blockchain</li>
+            <li className="Header-link">Distributed</li>
+            <li className="Header-link">Tokens</li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
