9d02137cc01b85a425fe071c04ab9ef32e72d1c4
glody007 <glodymbutwile@gmail.com>
2022-10-26 15:21:36 +0200

    feat: Hero section style.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Header />
       <div className='App-section Hero'>
         <div className='Hero-main'>
-          <h1>Understand how <span>Blockchain</span> works on just one page.</h1>
+          <h1>Understand how <span className='highlited'>Blockchain</span> works on just one page.</h1>
           <h4>Learn how Bitcoin works by understanding the blockchain, which is the technology behind it.</h4>
           <button className='button button--cta'>
             Learn
