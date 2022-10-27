f182ddc66a494a7057d96adb5303aaecddbdb47a
glody007 <glodymbutwile@gmail.com>
2022-10-27 21:04:04 +0200

    fix: Replace for property by htmlFor.

--- src/component/Block.js
+++ src/component/Block.js
@@ -72,32 +72,32 @@ function Block({
         ${stateAware && state === INVALID ? "block--invalid" : ""}`}>
         {showNumber && 
             <div className="block__row">
-                <label for="number" className="block__label">Block:</label>
+                <label htmlFor="number" className="block__label">Block:</label>
                 <input id="number" type="number" name="number" className="block__control" 
                     value={number} onChange={(event) => setNumber(event.target.value)} />
             </div>
         }
         {showNonce &&
             <div className="block__row">
-                <label for="nonce" className="block__label">Nonce:</label>
+                <label htmlFor="nonce" className="block__label">Nonce:</label>
                 <input id="nonce" type="text" name="nonce" className="block__control" 
                     value={nonce} onChange={(event) => setNonce(event.target.value)} />
             </div>
         }
         <div className="block__row">
-            <label for="data" className="block__label">Data:</label>
+            <label htmlFor="data" className="block__label">Data:</label>
             <textarea id="data" name="data" rows="10" className="block__control" 
                 value={data} onChange={(event) => setData(event.target.value)} />
         </div>
         {showPrev &&
             <div className="block__row">
-                <label for="prev" className="block__label">Prev:</label>
+                <label htmlFor="prev" className="block__label">Prev:</label>
                 <input id="prev" type="text" name="prev" className="block__control" 
                     disabled value={prev} />
             </div>
         }
         <div className="block__row">
-            <label for="hash" className="block__label">Hash:</label>
+            <label htmlFor="hash" className="block__label">Hash:</label>
             <input id="hash" type="text" name="hash" className="block__control" 
                 disabled value={hash} />
         </div>
