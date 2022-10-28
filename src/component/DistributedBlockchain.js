d91e0b01e06189e284d4e18a7cf36acabd622610
glody007 <glodymbutwile@gmail.com>
2022-10-28 07:41:13 +0200

    feat: Create distributed blockchain.

---
+++ src/component/DistributedBlockchain.js
@@ -0,0 +1,56 @@
+import './DistributedBlockchain.scss'
+import Blockchain from './Blockchain';
+import { v4 as uuidv4 } from 'uuid';
+import {
+    useState,
+    useEffect,
+    useCallback,
+    useMemo
+} from 'react'
+
+function DistributedBlockchain({ numberOfPeer }) {
+
+    const alphabet = useMemo(() => {
+        return [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+        'L','M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z' ]
+    }, []) 
+
+    const [peers, setPeers] = useState([])
+
+    // Helpers
+    const genatePeerName = useCallback((peerNumber) => {
+        const number = parseInt(peerNumber / alphabet.length)
+        const alphabetIndex = peerNumber % alphabet.length
+        return `${alphabet[alphabetIndex]}${number > 0 ? number : ''}`
+    }, [alphabet])
+
+    // Callbacks 
+    const createPeers = useCallback(() => {
+        const newPeerList = []
+        for(let i=0; i < numberOfPeer; i++) {
+            newPeerList.push({id: uuidv4(), name: genatePeerName(i)})
+        }
+        setPeers(newPeerList)
+    }, [numberOfPeer, genatePeerName])
+
+    useEffect(() => {
+        createPeers()
+    }, [createPeers])
+
+    return (
+        <div className='dist-blockchain'>
+            {peers.map((item) => (
+                <div className='peer'>
+                    <h3 className='peer__title'>{item.name}</h3>
+                    <Blockchain />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+DistributedBlockchain.defaultProps = {
+    numberOfPeer: 3
+}
+
+export default DistributedBlockchain;
--- src/component/index.js
+++ src/component/index.js
@@ -2,10 +2,12 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Block from "./Block";
 import Blockchain from "./Blockchain";
+import DistributedBlockchain from "./DistributedBlockchain";
 
 export {
     Header,
     Footer,
     Block,
-    Blockchain
+    Blockchain,
+    DistributedBlockchain
 }
